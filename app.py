f32f8c738e7ae80c1048a0a19979d513f8290f3c
Paul Fletes <paul.fletes@gmail.com>
2022-11-22 08:25:31 -0600

    update results route

--- app.py
+++ app.py
@@ -12,7 +12,32 @@ def main():
 
 @app.route('/results')
 def results():
-    pass
+    api_url = 'https://swapi.py4e.com/api/'
+    character_id = request.args.get('character_id')
+    response_people = request.get(api_url + 'people/' + character_id)
+    character = json.loads(response_people.content)
+    response_films = []
+
+    try:
+        films = character['films']
+        response_films = []
+        for film in films:
+            response_films.append(json.loads(requests.get(film).content))
+    except KeyError:
+        response_films
+
+    try:
+        response_homeworld = requests.get(character['homeworld'])
+        homeworld = json.loads(response_homeworld.content)
+    except KeyError:
+        homeworld = ''
+
+    context = {
+        'character': character,
+        'films': response_films,
+        'homeworld': homeworld,
+    }
+    return render_template('results.html', **context)
 
 
 if __name__ == '__main__':
